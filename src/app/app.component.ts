bf9371ee2969759571732bffdb2eac3a98aab475
radrabik <radoslav.drabik@gmail.com>
2021-07-19 22:41:42 +0200

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,23 @@
 import { Component, VERSION } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+
+  registrationForm = new FormGroup({
+    userName: new FormControl('Rado'),
+    password: new FormControl(''),
+    confirmPassword: new FormControl(''),
+    address: new FormGroup({
+        city: new FormControl(''),
+        state: new FormControl(''),
+        postalCode: new FormControl(''),
+    })
+
+  });
+
+}
