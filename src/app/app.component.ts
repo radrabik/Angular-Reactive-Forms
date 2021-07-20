c4ae31f61250212ac989cff82f3d0531435a6ee1
radrabik <radoslav.drabik@gmail.com>
2021-07-20 08:34:33 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,7 +21,26 @@ export class AppComponent {
   });
 
   loadApiData() {
-    
+
+    // set all values - mandatory to pass all values
+    this.registrationForm.setValue({
+      userName: 'Tomas with setValue',
+      password: '1cibula123',
+      confirmPassword: '1cibula123',
+      address: {
+        city: 'City',
+        state: 'State',
+        postalCode: '12345'
+      }
+    });
+
+    // set specific values - not all fields have to be passed
+    this.registrationForm.patchValue({
+      userName: 'Tomas with patchValue',
+      password: '1cibula123',
+      confirmPassword: '1cibula123',
+    });
+
   }
 
 }
